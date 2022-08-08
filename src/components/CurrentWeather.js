104f0413d7a7d495844cd489e12138d547c4c74c
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-08 16:34:31 +0430

    a new component created for showing weather icons and imprted in currentWeather component

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,17 +1,6 @@
 import React, {useContext} from 'react';
 
-//icons
-import clearDay from "../assets/icons/Clear-Day.svg";
-import clearNight from "../assets/icons/Clear-Night.svg";
-import clouds from "../assets/icons/Clouds.svg";
-import Rain from "../assets/icons/Rain.svg";
-import Snow from "../assets/icons/Snow.svg";
-import Drizzle from "../assets/icons/Drizzle.svg";
-import Mist from "../assets/icons/Mist.svg";
-import Thunderstorm from "../assets/icons/Thunderstorm.svg";
-
-//functions
-import { getDayOrNight } from '../helpers/functions';
+import Icon from './shared/Icon';
 
 //contexts
 import { LocationContext } from '../contexts/LocationContextProvider';
@@ -20,26 +9,11 @@ import { WeatherContext } from '../contexts/WeatherContextProvider';
 const CurrentWeather = () => {
     const locationData = useContext(LocationContext);
     const weatherData = useContext(WeatherContext);
-    
-    const dayOrNight = getDayOrNight()
-
-
-
-    console.log(dayOrNight);
-    
+ 
     return (
         <div>
             <h2>{locationData.timezone}</h2>
-            <div>
-                {Object.keys(weatherData).length !== 0 && weatherData.weather[0].main=== "Clouds" && <img src={clouds}alt='condition'/>}
-                {Object.keys(weatherData).length !== 0 && weatherData.weather[0].main=== "Rain" && <img src={Rain}alt='condition'/>}
-                {Object.keys(weatherData).length !== 0 && weatherData.weather[0].main=== "Snow" && <img src={Snow}alt='condition'/>}
-                {Object.keys(weatherData).length !== 0 && weatherData.weather[0].main=== "Mist" && <img src={Mist}alt='condition'/>}
-                {Object.keys(weatherData).length !== 0 && weatherData.weather[0].main=== "Drizzle" && <img src={Drizzle}alt='condition'/>}
-                {Object.keys(weatherData).length !== 0 && weatherData.weather[0].main=== "Thunderstorm" && <img src={Thunderstorm}alt='condition'/>}
-                {Object.keys(weatherData).length !== 0 && weatherData.weather[0].main=== "Clear-Day" && dayOrNight === "Day" && <img src={clearDay}alt='condition'/>  }
-                {Object.keys(weatherData).length !== 0 && weatherData.weather[0].main=== "Clear-Night" && dayOrNight === "Night" && <img src={clearNight}alt='condition'/>  }
-            </div>
+            <Icon />
 
         </div>
     );
