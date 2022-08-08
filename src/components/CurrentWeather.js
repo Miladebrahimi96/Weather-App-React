a64eefe97d830b69113d936b0f2fb5b734afc8f5
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-08 17:04:51 +0430

    degree in centigrade and fahrenheit is now visivle

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useRef} from 'react';
 
 import Icon from './shared/Icon';
 
@@ -6,16 +6,42 @@ import Icon from './shared/Icon';
 import { LocationContext } from '../contexts/LocationContextProvider';
 import { WeatherContext } from '../contexts/WeatherContextProvider';
 
+//functions
+import { centigrade } from '../helpers/functions';
+import { fahrenheit } from '../helpers/functions';
+
 const CurrentWeather = () => {
     const locationData = useContext(LocationContext);
     const weatherData = useContext(WeatherContext);
+    console.log(weatherData);
+
+    const degree = useRef();
+    
+    console.log(degree);
+
+    const clickHandler = () => {
+        if(degree.current.innerText.slice(-1) === "C"){
+            degree.current.innerText = `${fahrenheit(centigrade(weatherData.main.temp))}° F `;
+        }
+        else {
+            degree.current.innerText = `${centigrade(weatherData.main.temp)}° C `;
+        }
+    }
  
     return (
-        <div>
-            <h2>{locationData.timezone}</h2>
-            <Icon />
+        <>
+            <div>
+                <h2>{locationData.timezone}</h2>
+                <Icon />
+            </div>
+            <div>
+                {Object.keys(weatherData).length !== 0 && <h3 ref={degree} onClick={clickHandler}>{centigrade(weatherData.main.temp)}° C </h3>}
+                {Object.keys(weatherData).length !== 0 && <h3>{weatherData.weather[0].description} </h3>}
+                
+            </div>
+            
 
-        </div>
+        </>
     );
 };
 
--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -8,3 +8,13 @@ export const getDayOrNight = () => {
     }
     return dayOrNight
 }
+
+export const centigrade = degree => {
+    const newDegree = degree - 272.15;
+    return newDegree.toFixed(2)
+}
+
+export const fahrenheit = degree => {
+    const newDegree = (degree * 1.8)  +32;
+    return newDegree.toFixed(2);
+}
