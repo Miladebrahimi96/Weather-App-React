9e2698624657c8678964922f8e07078d285c10b4
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-09 16:22:13 +0430

    A ForcastContext provided was made to save Forcast data from getForcast function

---
+++ src/contexts/ForcastContextProvider.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect, useContext, createContext} from 'react';
+
+//API
+import { getForcast } from '../services/api';
+
+//Context
+import { LocationContext } from './LocationContextProvider';
+
+const ForcastContext = createContext();
+
+const ForcastContextProvider = ({children}) => {
+
+    const [forcast, setForcast] = useState({})
+
+    const location = useContext(LocationContext);
+    const lat = location.lat;
+    const lon = location.lon;
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setForcast(await getForcast(lat,lon));
+        }
+        if(lat && lon){
+            fetchAPI();
+        }
+    }, [lat, lon])
+
+    return (
+        <>
+        {Object.keys(forcast).length !== 0 &&
+            <ForcastContext.Provider value={forcast}>
+                {children}
+            </ForcastContext.Provider>
+        }
+        </>
+    );
+};
+
+export default ForcastContextProvider;
