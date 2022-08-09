988f8531c6688d4c5208e740ca69d208eef5c9b2
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-09 14:07:23 +0430

    A component called WeatherDetails created for showing extra details

---
+++ src/components/shared/WeatherDetails.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react';
+
+//Contexts
+import { WeatherContext } from '../../contexts/WeatherContextProvider';
+
+//Functions
+import { centigrade } from '../../helpers/functions';
+
+const WeatherDetails = () => {
+
+    const weatherData = useContext(WeatherContext);
+
+    return (
+        <div>
+            <p>Max Temperature: {Object.keys(weatherData).length !== 0 && <span>{centigrade(weatherData.main.temp_max)}° C</span>}</p>
+            <p>Min Temperature: {Object.keys(weatherData).length !== 0 && <span>{centigrade(weatherData.main.temp_min)}° C</span>}</p>
+            <p>Humidity: {Object.keys(weatherData).length !== 0 && <span>{weatherData.main.humidity}%</span>}</p>
+            <p>Pressure: {Object.keys(weatherData).length !== 0 && <span>{weatherData.main.pressure}mbar</span>}</p>
+            <p>Wind Speed: {Object.keys(weatherData).length !== 0 && <span>{weatherData.wind.speed}km/h</span>}</p>
+        </div>
+    );
+};
+
+export default WeatherDetails;
